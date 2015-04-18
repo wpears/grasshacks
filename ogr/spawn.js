c181cc99c2cc42d6ace6887ecf35c805cf15ec3d
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-04-17 21:55:41 -0400

    Shp also instant

--- ogr/spawn.js
+++ ogr/spawn.js
@@ -5,10 +5,12 @@ console.time('parse');
 console.time('good');
 console.time('firstByte');
 console.time('gdb');
+console.time('shp');
 
 var child = spawn('ogr2ogr', ['-f', 'GeoJson', 'badout.txt', '/vsistdin/']);
 var goodChild = spawn('ogr2ogr', ['-f', 'GeoJson', '/vsistdout/', '/vsistdin/']);
 var gdbChild = spawn('ogr2ogr', ['-f', 'GeoJson', '/vsistdout/', 'utah.gdb']);
+var shpChild = spawn('ogr2ogr', ['-f', 'GeoJson', '/vsistdout/', 'arkansas/STRUC_SITUS_ADDRESS_PT.shp']);
 
 var stream = fs.createReadStream('arkansas.json')
 var badStream = fs.createReadStream('badArkansas.json')
@@ -23,6 +25,11 @@ goodChild.on('exit', function(){
 
 goodChild.stdout.once('data', function(d){
   console.timeEnd('firstByte');
+})
+
+shpChild.stdout.once('data', function(d){
+  console.timeEnd('shp');
+  shpChild.kill();
 })
 
 gdbChild.stdout.once('data', function(d){
