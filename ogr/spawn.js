4c112221a28eeaa1168090209b14eb376e5b55f6
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-04-21 09:09:51 -0400

    Reformat clarify test cases

--- ogr/spawn.js
+++ ogr/spawn.js
@@ -1,41 +1,44 @@
 var spawn = require('child_process').spawn;
 var fs = require('fs');
 
-console.time('parse');
-console.time('good');
-console.time('firstByte');
-console.time('gdb');
-console.time('shp');
-
+console.time('parse fails');
+console.time('streamed json finished');
+console.time('firstByte from json');
+console.time('utah.gdb');
+console.time('arkansas/STRUC_SITUS_ADDRESS_PT.shp');
+console.time('/vsizip/arkansas.zip')
 var child = spawn('ogr2ogr', ['-f', 'GeoJson', 'badout.txt', '/vsistdin/']);
 var goodChild = spawn('ogr2ogr', ['-f', 'GeoJson', '/vsistdout/', '/vsistdin/']);
-var gdbChild = spawn('ogr2ogr', ['-f', 'GeoJson', '/vsistdout/', 'utah.gdb']);
-var shpChild = spawn('ogr2ogr', ['-f', 'GeoJson', '/vsistdout/', 'arkansas/STRUC_SITUS_ADDRESS_PT.shp']);
 
 var stream = fs.createReadStream('arkansas.json')
 var badStream = fs.createReadStream('badArkansas.json')
 
 child.stderr.on('end', function(){
- console.timeEnd('parse'); 
+ console.timeEnd('parse fails'); 
 })
 
 goodChild.on('exit', function(){
-  console.timeEnd('good');
+  console.timeEnd('streamed json finished');
 })
 
 goodChild.stdout.once('data', function(d){
-  console.timeEnd('firstByte');
+  console.timeEnd('firstByte from json');
 })
 
-shpChild.stdout.once('data', function(d){
-  console.timeEnd('shp');
-  shpChild.kill();
-})
+var files = ['utah.gdb','arkansas/STRUC_SITUS_ADDRESS_PT.shp','/vsizip/arkansas.zip']
 
-gdbChild.stdout.once('data', function(d){
-  console.timeEnd('gdb');
-  gdbChild.kill();
-})
+files.forEach(function(v){
+  var currChild = spawn('ogr2ogr', ['-f', 'GeoJson', '/vsistdout/', v]);
+
+  currChild.stdout.once('data', function(d){
+    console.log(d.slice(0,250).toString());
+    console.timeEnd(v);
+    currChild.kill();
+  });
+
+  currChild.stderr.pipe(process.stdout);
+});
+
 
 stream.pipe(goodChild.stdin);
 badStream.pipe(child.stdin);
